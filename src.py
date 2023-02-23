8a8ebb4e663d92fa8a48ea6562c5a2e26298b7be
Andy Jung <jungkj@bc.edu>
2023-02-23 12:56:04 -0500

    testing oneill

--- src.py
+++ src.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 #Selenium code to open the browser
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/andyjung/Downloads/chromedriver_mac64 (1)')
 driver.get("https://libcal.bc.edu/reserve/oneill")
 
 #Testing selenium run
