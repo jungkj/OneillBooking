3172264ea02cb70b9e0680b4b4481dd26ad5ff88
Andy Jung <jungkj@bc.edu>
2023-02-23 12:58:52 -0500

    minor changes

--- src.py
+++ src.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 
 #Selenium code to open the browser
-driver = webdriver.Chrome(executable_path='/Users/andyjung/Downloads/chromedriver_mac64 (1)')
+driver = webdriver.Chrome(executable_path='/Users/andyjung/Downloads/chromdriver1')
+
+
 driver.get("https://libcal.bc.edu/reserve/oneill")
 
 #Testing selenium run
 print(driver.title)
+
+#Test
